b6f79bd769022d17f3c763b0dfc091726084b276
karthik198500 <karthik198500@gmail.com>
2022-07-09 12:54:49 +1000

    Project Initialization

---
+++ src/test/java/com/doordash/interview/phoneNumberParser/PhoneNumberParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doordash.interview.phoneNumberParser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneNumberParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
